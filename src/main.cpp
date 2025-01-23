ddebf36d860b92d963d0b7d7a55bf9570a0a6240
JetShen <fernandoarielthielelepe@gmail.com>
2025-01-22 22:14:15 -0300

    Update CMake configuration and enhance main.cpp with shader management and 3D rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <cstdlib>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+void checkShaderCompileStatus(unsigned int shader) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        fprintf(stderr, "Error al compilar el shader: %s\n", infoLog);
+    }
+}
+
+void checkProgramLinkStatus(unsigned int program) {
+    int success;
+    char infoLog[512];
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        fprintf(stderr, "Error al enlazar el programa de shaders: %s\n", infoLog);
+    }
 }
 
 int main() {
@@ -16,7 +41,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Mi Ventana OpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Cubo 3D", NULL, NULL);
     if (!window) {
         fprintf(stderr, "Failed to create GLFW window\n");
         glfwTerminate();
@@ -31,12 +56,106 @@ int main() {
         return -1;
     }
 
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f,
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f};
+    unsigned int indices[] = {
+        0, 1, 2, 2, 3, 0, 1, 5, 6, 6, 2, 1, 5, 4, 7, 7, 6, 5,
+        4, 0, 3, 3, 7, 4, 3, 2, 6, 6, 7, 3, 4, 5, 1, 1, 0, 4};
+
+    // Configurar VBO, VAO y EBO
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    const char *vertexShaderPath = "/shaders/vertex_shader.glsl";
+    const char *fragmentShaderPath = "/shaders/fragment_shader.glsl";
+
+    FILE *vertexShaderFile = fopen(vertexShaderPath, "r");
+    if (!vertexShaderFile) {
+        fprintf(stderr, "Error al abrir el vertex shader\n");
+        return -1;
+    }
+    fseek(vertexShaderFile, 0, SEEK_END);
+    long vertexShaderSize = ftell(vertexShaderFile);
+    fseek(vertexShaderFile, 0, SEEK_SET);
+    char *vertexShaderSource = (char *)malloc(vertexShaderSize + 1);
+    fread(vertexShaderSource, 1, vertexShaderSize, vertexShaderFile);
+    vertexShaderSource[vertexShaderSize] = '\0';
+    fclose(vertexShaderFile);
+
+    FILE *fragmentShaderFile = fopen(fragmentShaderPath, "r");
+    if (!fragmentShaderFile) {
+        fprintf(stderr, "Error al abrir el fragment shader\n");
+        return -1;
+    }
+    fseek(fragmentShaderFile, 0, SEEK_END);
+    long fragmentShaderSize = ftell(fragmentShaderFile);
+    fseek(fragmentShaderFile, 0, SEEK_SET);
+    char *fragmentShaderSource = (char *)malloc(fragmentShaderSize + 1);
+    fread(fragmentShaderSource, 1, fragmentShaderSize, fragmentShaderFile);
+    fragmentShaderSource[fragmentShaderSize] = '\0';
+    fclose(fragmentShaderFile);
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, (const char **)&vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompileStatus(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, (const char **)&fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompileStatus(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkProgramLinkStatus(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+    glUseProgram(shaderProgram);
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
