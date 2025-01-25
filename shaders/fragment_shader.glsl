4c0e16c3cbd393dd8a83f83d490d1102d5419885
JetShen <fernandoarielthielelepe@gmail.com>
2025-01-24 21:34:48 -0300

    Enhance fragment shader to include edge detection logic

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,6 +1,14 @@
 #version 330 core
 out vec4 FragColor;
-
+// don't work :D
 void main() {
-    FragColor = vec4(1.0, 0.5, 0.2, 1.0); // Color naranja
+    vec2 fragCoord = gl_FragCoord.xy / vec2(800.0, 600.0);
+
+    float edgeThreshold = 0.005;
+    if (fragCoord.x < edgeThreshold || fragCoord.x > 1.0 - edgeThreshold ||
+        fragCoord.y < edgeThreshold || fragCoord.y > 1.0 - edgeThreshold) {
+        FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+    }
 }
